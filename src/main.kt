cb9742a83ffbb0bb78747b584fd8e289e4034d2e
Ibrahim Alashqar <i.alashqar@ostfalia.de>
2023-05-13 15:43:55 +0200

    commited und aktualisiert

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,8 @@
 fun main() {
     print("Hello Kotlin")
     println("Hallo Kotlin Line")
+    println("Hallo Kotlin Line")
+    println("Hallo Kotlin Line")
+    println("Hallo Kotlin Line")
+    println("Hallo Kotlin Line")
 }
