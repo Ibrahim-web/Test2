385107833ba8daae97a72d1b5c05da1e5f82a87a
Ibrahim Alashqar <i.alashqar@ostfalia.de>
2023-05-14 09:46:27 +0200

    Datatyp

--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,20 @@
 fun main() {
-    print("Hello Kotlin")
-    println("Hallo Kotlin Line")
-    println("Hallo Kotlin Line")
-    println("Hallo Kotlin Line")
-    println("Hallo Kotlin Line")
-    println("Hallo Kotlin Line")
+    var firstnName = "Ibrahim"
+    var lastName= "Alashqar"
+
+    var car : String = "BMW"
+    var car2 : String = "BMW"
+
+    println(lastName)
+    println(car2::class)
+
+    var number : Int = 25
+    println(number::class.qualifiedName)
+    println("==================================================")
+    val x = 21
+    val name: String = "Val Keyword"
+
+    println(firstnName.isEmpty())
+    println(name)
+
 }
